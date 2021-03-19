569ea4b1627f9b27d303238e04729bd7b72daf51
Anthony Li <anthonyli358@gmail.com>
2021-03-19 14:31:18 +0000

    Initial API call

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Exploration implementing recommender systems using Spotify data.
 - Popularity
 - Popularity + Hybrid
 - Genre Weighting
+
+- https://developer.spotify.com/documentation/web-api/reference/
+- https://spotipy.readthedocs.io/
+- https://developer.spotify.com/dashboard/applications
+- https://developer.spotify.com/documentation/general/guides/authorization-guide/#list-of-scopes
--- music_data.py
+++ music_data.py
@@ -1,11 +1,24 @@
 import spotipy
+import yaml
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
+with open("spotify/client_details.yml", 'r') as stream:
+    client_details = yaml.safe_load(stream)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+scope = "user-library-read user-follow-read user-top-read playlist-read-private playlist-modify-private"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=client_details['client_id'],
+    client_secret=client_details['client_secret'],
+    redirect_uri=client_details['redirect_uri'],
+    scope=scope,
+))
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+playlists = sp.current_user_playlists()
+
+playlist_items = sp.playlist_items('')
